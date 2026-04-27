3ed9ad52bb7e5c2ab6c14d9d0b82a122863a04b6
Jon Martirosyan <jonmartirosyan1988@gmail.com>
2026-04-27 11:42:56 +0400

    CTNT-456 chore: changed v1 to v2 as spec says

--- mock-services/rover-mock/src/app.ts
+++ mock-services/rover-mock/src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+
 import { artificialDelay } from './middleware/delay';
 import { authorsRouter } from './modules/authors';
 import { contentRouter } from './modules/content';
@@ -10,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(artificialDelay);
 
-app.use('/v1/authors', authorsRouter);
-app.use('/v1/content', contentRouter);
+app.use('/v2/authors', authorsRouter);
+app.use('/v2/content', contentRouter);
 
 app.get('/health', (_req, res) => {
   res.json({ status: 'ok', service: 'rover-mock' });
